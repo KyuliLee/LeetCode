6e6d5b0a87291381ee9c7c3cf27628a9034fa905
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2025-01-19 22:32:23 +0900

    Time: 1 ms (4.24%), Space: 42.8 MB (33.13%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            System.out.println("HI");
+        }
+
+        if(list1 == null && list2 == null) {
+            return null;
+        }
+        if(list1 == null && list2 != null) {
+            return list2;
+        }
+        if(list1 != null && list2 == null) {
+            return list1;
+        }
+        ListNode ans = new ListNode(); // 리턴할 노드
+        // ans노드 초기화
+        ListNode currNode1 = list1;
+        ListNode currNode2 = list2;
+        if(currNode1.val <= currNode2.val) {
+            ans.val = currNode1.val;
+        } else {
+            ans.val = currNode2.val;
+        }
+        
+        ListNode lastNode = ans; // 임시 노드
+
+        // curr1 또는 curr2이 없을 때까지 반복
+        boolean isList1End = false;
+        boolean isList2End = false;
+        while(true){
+            if(currNode1 == null) {
+                isList1End = true;
+                lastNode.next = currNode2;
+                break;
+            }
+            if(currNode2 == null) {
+                isList1End = true;
+                lastNode.next = currNode1;
+                break;
+            }
+            int val1 = currNode1.val;
+            int val2 = currNode2.val;
+            if(val1 <= val2) {
+                ListNode newNode = new ListNode(val1);
+                lastNode.next = newNode;
+                lastNode = lastNode.next;
+                currNode1 = currNode1.next;
+            } else {
+                ListNode newNode = new ListNode(val2);
+                lastNode.next = newNode;
+                lastNode = lastNode.next;
+                currNode2 = currNode2.next;
+            }
+
+        }
+        ans = ans.next;
+        return ans;
+    }
+}
